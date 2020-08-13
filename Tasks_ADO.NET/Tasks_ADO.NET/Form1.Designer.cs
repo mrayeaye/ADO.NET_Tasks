5c303360e1c56256dfc435adeefdc3921a2dad6d
Ahmed Saad <ahmedsaad17391739@gmail.com>
2020-08-13 08:01:34 -0400

    DataDridView shows data whenever I add a new task and remove task is now fuunctional

--- Tasks_ADO.NET/Tasks_ADO.NET/Form1.Designer.cs
+++ Tasks_ADO.NET/Tasks_ADO.NET/Form1.Designer.cs
@@ -30,15 +30,14 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.task = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.stat = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stat = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.task = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -54,7 +53,6 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.stat);
@@ -71,15 +69,59 @@
             this.groupBox1.Text = "Task";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label1
+            // button2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Task:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(434, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 25);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status";
+            // 
+            // stat
+            // 
+            this.stat.FormattingEnabled = true;
+            this.stat.Location = new System.Drawing.Point(64, 84);
+            this.stat.Name = "stat";
+            this.stat.Size = new System.Drawing.Size(124, 21);
+            this.stat.TabIndex = 6;
+            this.stat.SelectedIndexChanged += new System.EventHandler(this.stat_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(371, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Date:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // task
+            // 
+            this.task.Location = new System.Drawing.Point(50, 35);
+            this.task.Name = "task";
+            this.task.Size = new System.Drawing.Size(186, 20);
+            this.task.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(422, 35);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(130, 20);
+            this.dateTimePicker1.TabIndex = 2;
             // 
             // button1
             // 
@@ -92,68 +134,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dateTimePicker1
+            // label1
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(422, 35);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(130, 20);
-            this.dateTimePicker1.TabIndex = 2;
-            // 
-            // task
-            // 
-            this.task.Location = new System.Drawing.Point(50, 35);
-            this.task.Name = "task";
-            this.task.Size = new System.Drawing.Size(186, 20);
-            this.task.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(371, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Date:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // stat
-            // 
-            this.stat.FormattingEnabled = true;
-            this.stat.Location = new System.Drawing.Point(64, 84);
-            this.stat.Name = "stat";
-            this.stat.Size = new System.Drawing.Size(124, 21);
-            this.stat.TabIndex = 6;
-            this.stat.SelectedIndexChanged += new System.EventHandler(this.stat_SelectedIndexChanged);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 87);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Status";
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(434, 87);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 25);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Remove";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(328, 87);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 25);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Edit";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Task:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
@@ -181,7 +170,6 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label3;
     }
--- Tasks_ADO.NET/Tasks_ADO.NET/Form1.cs
+++ Tasks_ADO.NET/Tasks_ADO.NET/Form1.cs
@@ -22,26 +22,9 @@ namespace Tasks_ADO.NET
             stat.Items.Add("In Progress");
             stat.Items.Add("Done");
 
-            BindingSource bi = new BindingSource();
-            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
-            try
-            {
-
-                //SqlCommand cmd = new SqlCommand($"Insert Into tasks (Name,Status,Date) Values({task.Text},{stat.GetItemText(this.stat.SelectedItem)},{dateTimePicker1.Value}) ", con);
-                SqlCommand cmd = new SqlCommand("Select * from tasksV2", con);
-                con.Open();
-                SqlCommand check_sql = new SqlCommand("Select Count(Id) from tasksV2", con);
-                int rows_inDB = (int)check_sql.ExecuteScalar();
-
-                if (rows_inDB > 0) { 
-                SqlDataReader rdr = cmd.ExecuteReader();
-                bi.DataSource = rdr;
-                dataGridView1.DataSource = bi;
-                dataGridView1.Refresh();
-                    }
-            }
-            catch { Console.WriteLine("EXCEPTION"); }
-            finally { con.Close(); }
+            // To view all database rows when application is started
+            RefreshData();
+            
         }
 
 
@@ -70,6 +53,31 @@ namespace Tasks_ADO.NET
 
         }
 
+        // method to refresh the gridview and gets called whenever necessary
+        private void RefreshData()
+        {
+            BindingSource bi = new BindingSource();
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+            try
+            {
+
+                //SqlCommand cmd = new SqlCommand($"Insert Into tasks (Name,Status,Date) Values({task.Text},{stat.GetItemText(this.stat.SelectedItem)},{dateTimePicker1.Value}) ", con);
+                SqlCommand cmd = new SqlCommand("Select * from tasksV2", con);
+                con.Open();
+                SqlCommand check_sql = new SqlCommand("Select Count(Id) from tasksV2", con);
+                int rows_inDB = (int)check_sql.ExecuteScalar();
+
+                if (rows_inDB > 0)
+                {
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    bi.DataSource = rdr;
+                    dataGridView1.DataSource = bi;
+                    dataGridView1.Refresh();
+                }
+            }
+            catch { Console.WriteLine("EXCEPTION"); }
+            finally { con.Close(); }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString))
@@ -78,12 +86,24 @@ namespace Tasks_ADO.NET
                 con.Open();
                 int rows_inserted = cmd.ExecuteNonQuery();
                 Console.WriteLine(rows_inserted);
+                RefreshData();
             }
         }
 
         private void stat_SelectedIndexChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            using(SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand($"Delete from tasksV2 where Id={dataGridView1.SelectedCells[0].Value}", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                RefreshData();
+            }
         }
     }
 }
